3297718dbd0e14d1849d467ded7177b6948a6b97
aksalmek <48769108+aksalmek@users.noreply.github.com>
2019-08-09 02:41:20 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="//2018/09/practice-environmental-qwareness-to-our.html"}
-if(x=="2"){xxx="/2018/09/advantages-of-being-home-business-owner.html"}
-if(x=="3"){xxx="/2018/09/10-tips-for-start-small-business-startup.html"}
-if(x=="5"){xxx="/2018/09/the-way-to-start-business-at-home.html"}a_to_ve[i].href="https://tipsku05.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2019/08/top-5-challenges-in-manufacturing.html"}
+if(x=="2"){xxx="/2019/08/how-to-perform-windshield-repair-and.html"}
+if(x=="3"){xxx="/2019/08/aftermarket-services.html"}
+if(x=="5"){xxx="/2019/08/how-modern-insurance-policy-systems-are.html"}a_to_ve[i].href="https://nimepackfile.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
